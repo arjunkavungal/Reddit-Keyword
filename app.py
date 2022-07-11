232ac86face32602adb050f9e4809ca7563896b3
Arjun Kavungal <arjunkavungal@gmail.com>
2022-07-10 22:05:19 -0400

    turn things to functions

--- app.py
+++ app.py
@@ -9,84 +9,30 @@ import matplotlib.pyplot as plt
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import model
+from model import *
 import pickle
 nltk.download('punkt')
 nltk.download('stopwords')
 plt.switch_backend('Agg') 
-reddit = praw.Reddit(client_id="p1lt136fs51SWOv6zlM6QA",client_secret="Ffth4WUUPhmFO4_b6oUdlmY5e3ZOYA",
-                     username="Hot-Helicopter5986",password="",user_agent="a")
 @app.route("/")
 def home():
     subreddit = reddit.subreddit('python')
     hot = subreddit.hot(limit=20)
-    df = pd.DataFrame()
-    for i in hot:
-        df = df.append({'title': i.title,'ups':i.ups}, ignore_index=True)
+    df = get_hot_titles()
 
     model = pickle.load(open('model.pkl', 'rb'))
     model.fit(df.title,df.ups)
     model.predict(['python'])
     
-    df['Title length'] = len(df['title'])
-    for i in range(len(df)):
-        df['Title length'][i] = len(df['title'][i])
+    df = get_title_length(df)
 
-    q_low = df["Title length"].quantile(0.01)
-    q_hi  = df["Title length"].quantile(0.99)
-
-    df_filtered = df[(df["Title length"] < q_hi) & (df["Title length"] > q_low)]
-    plt.scatter(df_filtered['Title length'],df_filtered.ups)
-    plt.show()
-    a = []
-    for i in range(len(df)):
-        a.append(len(df['title'][i].split(' ')))
-    df['Word count'] = a   
-    fig = plt.figure(figsize=(10, 4))
-    plt.scatter(df['Word count'],df.ups)
-    plt.xlabel("Word count")
-    plt.ylabel("Number of Upvotes")
-    plt.title("Effect of Word Count on Number of Upvotes")
-
-    s = ""
-    for i in range(len(df)):
-        s += df['title'][i] + " "
-
-
-
-
-    text_tokens = word_tokenize(s)
-
-    tokens_without_sw = [word.lower() for word in text_tokens if not word.lower() in stopwords.words()]
-
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    for ele in s:
-        if ele in punc:
-            s = s.replace(ele, "")
-
-    s = ' '.join(tokens_without_sw)
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    for ele in s:
-        if ele in punc:
-            s = s.replace(ele, "")
-    df['title'] = df['title'].astype(str)
-    df["count"]= df["title"].str.get(0)
-    df = df.iloc[:20,:4]
-    df['title'].map(lambda x: x.lower() if isinstance(x,str) else x)
-
-    for j in range(len(df)):
-        for i in s.split(' '):
-            df.at[j,i] = df['title'][j].count(i)
-    df = df.append(df.sum().rename('Total'))
-    df.iloc[-1][4:-1].astype(int).nlargest(25).plot(kind="bar")
-    plt.rcParams["figure.figsize"] = (6,6)
-    plt.show()
-    for i in df.columns[4:]:
-        df[i] = df[i] * df['ups']
-    df = df.append(df.sum().rename('Total'))
-    df.iloc[-1][4:-1].astype(int).nlargest(25).plot(kind="bar")
-    plt.rcParams["figure.figsize"] = (6,6)
-    plt.show()
+    title_length_graph(df)
+    df = get_word_count(df)
+   
+    graph_scatter_plot(df, 'Word count', 'ups', "Word count", "Number of Upvotes","Effect of Word Count on Number of Upvotes ")
+    df = unweighted_word_count(df)
+  
+    plot_weighted_keywords(df)
     s = ""
     s += df['title'][1]
     return str(model.predict(['python'])[0])
